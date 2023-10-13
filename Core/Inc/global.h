170d9f032298c4791db9977ca50fe656e38b6c0e
Vinh <vinh.caok21@hcmut.edu.vn>
2023-10-13 08:24:48 +0700

    ex1

---
+++ Core/Inc/global.h
@@ -0,0 +1,21 @@
+/*
+ * global.h
+ *
+ *  Created on: Sep 28, 2023
+ *      Author: Duc Vinh
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "software_timer.h"
+#include "main.h"
+
+#define LED_1 1
+#define LED_2 2
+#define LED_3 3
+#define LED_4 4
+extern int status;
+
+
+#endif /* INC_GLOBAL_H_ */
---
+++ Core/Src/global.c
@@ -0,0 +1,9 @@
+/*
+ * global.c
+ *
+ *  Created on: Sep 28, 2023
+ *      Author: Duc Vinh
+ */
+#include "global.h"
+
+int status = LED_1;
